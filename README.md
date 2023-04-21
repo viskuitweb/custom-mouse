faf20c36e492bec368db18e37462ba0ee842184f
Viskuit <viskuitprojects@gmail.com>
2023-04-21 11:27:02 +0200

    Made animation and integrated mouseOut cursor hide

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The goal is to make a custom pointer with CSS and JS
 
 ## How it works
 
-div that follows the mouse every time it moves
+div that follows the mouse every time it moves. When the cursor leaves the screen the cursor's div becomes 0x0px, so it seems like it desapears.
 
 
 Shout out to Samson Omojola for the idea: https://blog.logrocket.com/creating-custom-mouse-cursor-css/
--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -1,11 +1,23 @@
 const customCursor = document.getElementById("custom-cursor");
 
-const cursorPosition = (e)=> {
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
+const cursorPosition = (event)=> {
+  const mouseY = event.clientY;
+  const mouseX = event.clientX;
      
-    customCursor.style.left = `${mouseX}px`;
-    customCursor.style.top = `${mouseY}px`;
+  customCursor.style.left = `${mouseX}px`;
+  customCursor.style.top = `${mouseY}px`;
+  cursorOpacity(14)
   }
 
-  window.addEventListener('mousemove', cursorPosition)
+const mouseOut = (() => {
+  cursorOpacity(0)
+})
+
+const cursorOpacity = (cursorWidth) => {
+  customCursor.style.width = (`${cursorWidth}px`);
+  customCursor.style.height = (`${cursorWidth}px`);
+}
+
+window.addEventListener("mouseout", mouseOut)
+
+window.addEventListener('mousemove', cursorPosition)
